d782de122d0baf17c737d0c77d901bc58878e1eb
Volodymyr Deryd <volodymyr.deryd@justanswer.com>
2020-03-16 17:10:17 +0200

    added navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
 import { NavBar } from "./src/NavBar";
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from './src/HomeScreen'
+import { DetailsScreen } from './src/DetailsScreen'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <NavBar title="furniture app"></NavBar>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Details" component={DetailsScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+const Stack = createStackNavigator();
 
 const styles = StyleSheet.create({
   container: {},
